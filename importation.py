7c3cbf57c406b0fa4778c603f0a58ea48e2df726
modestesmv <modesteemmanuel.somavo@ensae.fr>
2024-12-09 22:21:01 +0000

    Sauvegarde des modifications de importation.py

--- importation.py
+++ importation.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import yfinance as yf
 
+
+
 def process_expirations(symbols, expiration_dates=None):
     """
     Télécharge les données d'options pour une ou plusieurs dates d'expiration
@@ -22,14 +24,14 @@ def process_expirations(symbols, expiration_dates=None):
     for sym in symbols:  # Utiliser 'sym' au lieu de 'symbol' pour éviter toute confusion
         # Récupération des dates d'expiration disponibles pour le symbole
         ticker_obj = yf.Ticker(sym)
-        available_expirations = ticker_obj.options
+        available_exp = ticker_obj.options
 
         # Si expiration_dates est vide, utiliser toutes les dates disponibles
         if not expiration_dates:
-            expirations_to_process = available_expirations
+            expirations_to_process = available_exp
         else:
             # Vérifier que les dates fournies sont disponibles pour le symbole
-            expirations_to_process = [date for date in expiration_dates if date in available_expirations]
+            expirations_to_process = [date for date in expiration_dates if date in available_exp]
             if not expirations_to_process:
                 print(f"Aucune des dates d'expiration fournies n'est disponible pour {sym}.")
                 continue
@@ -38,7 +40,7 @@ def process_expirations(symbols, expiration_dates=None):
             try:
                 options = ticker_obj.option_chain(expiration_date)
             except Exception as e:
-                print(f"Erreur lors de la récupération des données pour {sym} à la date {expiration_date}: {e}")
+                print(f"Erreur pour {sym} à la date {expiration_date}: {e}")
                 continue  # Passer à la date suivante en cas d'erreur
 
             # Séparation des calls et des puts
@@ -59,7 +61,7 @@ def process_expirations(symbols, expiration_dates=None):
             symbol_data['expiration_date'] = pd.to_datetime(expiration_date)
 
             # Sélection des colonnes d'intérêt
-            symbol_data = symbol_data[['contractSymbol', 'impliedVolatility','strike', 'bid', 'ask', 'ticker', 'expiration_date', 'optionType','lastPrice']]
+            symbol_data = symbol_data[['contractSymbol', 'impliedVolatility', 'strike', 'bid', 'ask', 'ticker', 'expiration_date', 'optionType', 'lastPrice']]
 
             # Conversion des colonnes numériques
             numeric_cols = ['strike', 'bid', 'ask']
